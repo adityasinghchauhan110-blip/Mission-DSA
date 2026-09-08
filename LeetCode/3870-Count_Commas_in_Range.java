dd3a939b4120de93bab719db047f59f219536946
Aditya Singh <adityasinghchauhan110@gmail.com>
2026-09-08 09:56:26 +0530

    Create 3870-Count_Commas_in_Range.java

---
+++ LeetCode/3870-Count_Commas_in_Range.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countCommas(int n) {
+        int count = 0;
+        long threshold = 1000;
+        
+        while (n >= threshold) {
+            count += (n - threshold + 1);
+            threshold *= 1000;
+        }
+        
+        return count;
+    }
+}
